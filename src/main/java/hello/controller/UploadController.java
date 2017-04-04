6c42a1ccd72cae679136c6af19648739d13de853
Dramaticlly <steveiszhy@gmail.com>
2017-04-04 01:11:54 -0400

    adding jpeg image serve

--- src/main/java/hello/controller/UploadController.java
+++ src/main/java/hello/controller/UploadController.java
@@ -3,9 +3,12 @@ package hello.controller;
 import hello.storage.StorageFileNotFoundException;
 import hello.storage.StorageService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.connector.Response;
+import org.codehaus.plexus.util.IOUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,7 +17,9 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.stream.Collectors;
 
 
@@ -44,13 +49,25 @@ public class UploadController {
 
     // on success return status 200
     // filename in HTTP header, file content in HTTP body
-    @GetMapping("/store/files/{filename:.+}")
+    @GetMapping("/downloads/files/{filename:.+}")
     @ResponseBody
-    public ResponseEntity<Resource> serveFile(@PathVariable String filename){
+    public ResponseEntity<Resource> downloadFile(@PathVariable String filename){
+        log.info("trying to download filename: " + filename);
         Resource file = storageService.loadAsResource(filename);
         return ResponseEntity.ok()
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename = "+file.getFilename() + "/")
                 .body(file);
+    }
+
+    //File uploads = new File(properties.getProperty("upload.location"));
+    @GetMapping("/store/files/{filename:.+}")
+    @ResponseBody
+    public void serveFile(@PathVariable String filename, HttpServletResponse response) throws IOException {
+        log.info("trying to server filename: "+filename);
+        Resource file = storageService.loadAsResource(filename);
+        InputStream in = file.getInputStream();
+        response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+        IOUtil.copy(in, response.getOutputStream());
     }
 
     // curl -F "file=@new.pdf" localhost:8080/store
